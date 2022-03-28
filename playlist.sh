fac3611c3e184b3e0fb5956939a2512fa952d6c6
Zhang <yzhang@edmunds.com>
2022-03-27 17:40:04 -0700

    generate video playlist by script

---
+++ playlist.sh
@@ -0,0 +1,55 @@
+id=132;title='舞力全開【第132集】明星夫妻國標對抗賽特別企劃&性感舞姬爭霸賽特別企劃!';youtubeId=QA4RfuBYr20 erb playlist.html.erb > docs/playlist/episode/334/明星/132/index.html
+id=135;title='舞力全開【第135集】明星舞王舞后爭霸賽!';  youtubeId=GX-RVoai3XQ erb playlist.html.erb > docs/playlist/episode/334/明星/135/index.html
+id=136;title='舞力全開【第136集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-恰恰)!';youtubeId=y_RwhDh2loE erb playlist.html.erb > docs/playlist/episode/334/明星/136/index.html
+id=137;title='舞力全開【第137集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-森巴)!';youtubeId=5DmoVt9osWs erb playlist.html.erb > docs/playlist/episode/334/明星/137/index.html
+id=138;title='舞力全開【第138集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-倫巴)!';youtubeId=AX2pm1oVFOc erb playlist.html.erb > docs/playlist/episode/334/明星/138/index.html
+id=139;title='舞力全開【第139集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-捷舞)!';youtubeId=n-76ZIp_jns erb playlist.html.erb > docs/playlist/episode/334/明星/139/index.html
+id=140;title='舞力全開【第140集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-鬥牛)!';youtubeId=lLVPP8857fI erb playlist.html.erb > docs/playlist/episode/334/明星/140/index.html
+id=141;title='舞力全開【第141集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-森巴)!';youtubeId=tosPzZ5qxoE erb playlist.html.erb > docs/playlist/episode/334/明星/141/index.html
+id=142;title='舞力全開【第142集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-恰恰)!';youtubeId=d-pCjjGYpCc erb playlist.html.erb > docs/playlist/episode/334/明星/142/index.html
+id=143;title='舞力全開【第143集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-倫巴)!';youtubeId=mvl5DHZcQTw erb playlist.html.erb > docs/playlist/episode/334/明星/143/index.html
+id=144;title='舞力全開【第144集】明星舞王舞后爭霸賽';youtubeId=9l-E79MHwVc erb playlist.html.erb > docs/playlist/episode/334/明星/144/index.html
+id=145;title='舞力全開【第145集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-鬥牛)!';youtubeId=_0aL0Iv0lRw erb playlist.html.erb > docs/playlist/episode/334/明星/145/index.html
+id=146;title='舞力全開【第146集】明星舞王舞后爭霸賽!';  youtubeId=hSzMxLodPrY erb playlist.html.erb > docs/playlist/episode/334/明星/146/index.html
+id=147;title='舞力全開【第147集】明星舞王舞后爭霸賽!(敗部復活 再戰舞台)';  youtubeId=I6pVQMA8U8I erb playlist.html.erb > docs/playlist/episode/334/明星/147/index.html
+id=148;title='舞力全開【第148集】明星舞王舞后爭霸賽!(第一季冠軍賽)';  youtubeId=9wfQoiY_wgQ erb playlist.html.erb > docs/playlist/episode/334/明星/148/index.html
+id=149;title='舞力全開【第149集】明星舞王舞后爭霸賽!(第一季冠軍賽)';  youtubeId=ldw1I1xLasg erb playlist.html.erb > docs/playlist/episode/334/明星/149/index.html
+id=151;title='舞力全開【第151集】明星舞王舞后爭霸賽!';  youtubeId=rGS008CNa3Y erb playlist.html.erb > docs/playlist/episode/334/明星/151/index.html
+id=152;title='舞力全開【第152集】明星舞王舞后爭霸賽!';  youtubeId=luX5o1XchuI erb playlist.html.erb > docs/playlist/episode/334/明星/152/index.html
+id=153;title='舞力全開【第153集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-捷舞)!';youtubeId=UERprKFYXQA erb playlist.html.erb > docs/playlist/episode/334/明星/153/index.html
+id=154;title='舞力全開【第154集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-倫巴)!';youtubeId=-ar6XdXCnJU erb playlist.html.erb > docs/playlist/episode/334/明星/154/index.html
+id=155;title='舞力全開【第155集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-鬥牛)!';youtubeId=k6fiuYR6IRY erb playlist.html.erb > docs/playlist/episode/334/明星/155/index.html
+id=156;title='舞力全開【第156集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-恰恰)!';youtubeId=UayCTsS1Th4 erb playlist.html.erb > docs/playlist/episode/334/明星/156/index.html
+id=157;title='舞力全開【第157集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-森巴)!';youtubeId=I5DK8I6aUXM erb playlist.html.erb > docs/playlist/episode/334/明星/157/index.html
+id=158;title='舞力全開【第158集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-鬥牛)!';youtubeId=j_HlS8ULq9U erb playlist.html.erb > docs/playlist/episode/334/明星/158/index.html
+id=159;title='舞力全開【第159集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-捷舞)!';youtubeId=D1Kc6prikQ0 erb playlist.html.erb > docs/playlist/episode/334/明星/159/index.html
+id=160;title='舞力全開【第160集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-森巴)!';youtubeId=LkXc8kQM0sk erb playlist.html.erb > docs/playlist/episode/334/明星/160/index.html
+id=161;title='舞力全開【第161集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-倫巴)!';youtubeId=FpU9EH8wxTU erb playlist.html.erb > docs/playlist/episode/334/明星/161/index.html
+id=162;title='舞力全開【第162集】明星舞王舞后爭霸賽&拉丁五項國標舞王爭霸賽(指定舞科-恰恰)!';outubeId=HkmhSyFK230 erb playlist.html.erb > docs/playlist/episode/334/明星/162/index.html
+id=163;title='舞力全開【第163集】明星舞王舞后爭霸賽-敗部復活 再戰舞台!';youtubeId=czou1HNf6ck erb playlist.html.erb > docs/playlist/episode/334/明星/163/index.html
+id=164;title='舞力全開【第164集】明星舞王舞后爭霸賽-第二季冠軍賽!';youtubeId=qDg-fJstlzU erb playlist.html.erb > docs/playlist/episode/334/明星/164/index.html
+id=165;title='舞力全開【第165集】明星舞王舞后爭霸賽-第二季冠軍賽!';youtubeId=DeM1YRvtkdU erb playlist.html.erb > docs/playlist/episode/334/明星/165/index.html
+id=167;title='舞力全開【第167集】明星舞王舞后爭霸賽 - 第三季 !';  youtubeId=LlHPjLSgS2w erb playlist.html.erb > docs/playlist/episode/334/明星/167/index.html
+id=168;title='舞力全開【第168集】明星舞王舞后爭霸賽 - 第三季 & 雙人空中極限對抗賽特別企劃 !';youtubeId=7g33-UtvalM erb playlist.html.erb > docs/playlist/episode/334/明星/168/index.html
+id=169;title='舞力全開【第169集】明星舞王舞后爭霸賽 - 第三季 & 雙人空中極限對抗賽特別企劃 !';youtubeId=N82_8iZHe-g erb playlist.html.erb > docs/playlist/episode/334/明星/169/index.html
+id=170;title='舞力全開【第170集】明星舞王舞后爭霸賽 - 第三季 & 拉丁五項國標舞王爭霸賽 指定舞科-恰恰 !';youtubeId=NmfvI2i8Lw0 erb playlist.html.erb > docs/playlist/episode/334/明星/170/index.html
+id=171;title='舞力全開【第171集】明星舞王舞后爭霸賽 - 第三季 & 拉丁五項國標舞王爭霸賽 指定舞科-捷舞 !';youtubeId=ngY6TtmOdYA erb playlist.html.erb > docs/playlist/episode/334/明星/171/index.html
+id=172;title='舞力全開【第172集】明星舞王舞后爭霸賽 - 第三季 & 拉丁五項國標舞王爭霸賽 指定舞科-森巴 !';youtubeId=59rWktZaXpU erb playlist.html.erb > docs/playlist/episode/334/明星/172/index.html
+id=173;title='舞力全開【第173集】明星舞王舞后爭霸賽 - 第三季 & 團體組年度總冠軍特別表演 !';youtubeId=NUD2d2epXMo erb playlist.html.erb > docs/playlist/episode/334/明星/173/index.html
+id=174;title='舞力全開【第174集】明星舞王舞后爭霸賽 - 第三季 & 團體組年度總冠軍特別表演 !';youtubeId=_Gt8h1in_SU erb playlist.html.erb > docs/playlist/episode/334/明星/174/index.html
+id=175;title='舞力全開【第175集】明星舞王舞后爭霸賽 - 第三季 & 蘭陽森巴舞團特別表演 !';youtubeId=FVNiF98t-F8 erb playlist.html.erb > docs/playlist/episode/334/明星/175/index.html
+id=176;title='舞力全開【第176集】明星舞王舞后爭霸賽 - 第三季 & 世界冠軍 IP Lockers 特別表演 !';youtubeId=v5FhAMfNpUE erb playlist.html.erb > docs/playlist/episode/334/明星/176/index.html
+id=177;title='舞力全開【第177集】明星舞王舞后爭霸賽 - 第三季 & 永遠不嫌晚動感拉丁特別企劃 !';youtubeId=Lbi1HTEQq-U erb playlist.html.erb > docs/playlist/episode/334/明星/177/index.html
+id=178;title='舞力全開【第178集】明星舞王舞后爭霸賽 - 第三季 & 第二屆團體組年度總冠特別表演 - 創舞極致 !'; youtubeId=hNkv6MimGsE erb playlist.html.erb > docs/playlist/episode/334/明星/178/index.html
+id=179;title='舞力全開【第179集】明星舞王舞后爭霸賽 - 第三季 (敗部復活 再戰舞台) !'; youtubeId=7rAV-8KTmu4 erb playlist.html.erb > docs/playlist/episode/334/明星/179/index.html
+id=180;title='舞力全開【第180集】明星舞王舞后第三季冠軍賽 & 年度冠軍示範表演 !';  youtubeId=yxYpgeq0kPw erb playlist.html.erb > docs/playlist/episode/334/明星/180/index.html
+id=181;title='舞力全開【第181集】明星舞王舞后第三季冠軍賽 !';  youtubeId=1i9vbFfbjDQ erb playlist.html.erb > docs/playlist/episode/334/明星/181/index.html
+id=183;title='舞力全開【第183集】明星舞王舞后爭霸賽 - 年度總冠軍賽 !';youtubeId=NTF105dJd-Q erb playlist.html.erb > docs/playlist/episode/334/明星/183/index.html
+id=184;title='舞力全開【第184集】明星舞王舞后爭霸賽 - 年度總冠軍賽 !';youtubeId=0gH9JjCMtbA erb playlist.html.erb > docs/playlist/episode/334/明星/184/index.html
+id=185;title='舞力全開【第185集】明星舞王舞后爭霸賽 - 年度總冠軍賽 (8搶7淘汰賽)!'; youtubeId=pBye_YL-5jM erb playlist.html.erb > docs/playlist/episode/334/明星/185/index.html
+id=186;title='舞力全開【第186集】明星舞王舞后爭霸賽 - 年度總冠軍賽 (8搶7淘汰賽)!'; youtubeId=0jLlCdwUFfs erb playlist.html.erb > docs/playlist/episode/334/明星/186/index.html
+id=187;title='舞力全開【第187集】明星舞王舞后爭霸賽 - 年度總冠軍賽 (最終戰)!';  youtubeId=a2sazXYpVgM erb playlist.html.erb > docs/playlist/episode/334/明星/187/index.html
+id=188;title='舞力全開 愛妮雅化妝品【第188集】明星舞王舞后爭霸賽 - 年度總冠軍賽 (最終戰)!';youtubeId=TjJnSzZez8A erb playlist.html.erb > docs/playlist/episode/334/明星/188/index.html
+id=196;title='舞力全開 愛妮雅化妝品【第196集】明星舞王舞后爭霸賽!';youtubeId=F_2gUm-V1pU erb playlist.html.erb > docs/playlist/episode/334/明星/196/index.html
+id=197;title='舞力全開 愛妮雅化妝品【第197集】明星舞王舞后爭霸賽 & 台灣啦啦隊國家代表隊示範表演 !';youtubeId=kN_43__D45o erb playlist.html.erb > docs/playlist/episode/334/明星/197/index.html
+id=198;title='舞力全開 愛妮雅化妝品【第198集】明星舞王舞后爭霸賽 & 藝人獎金挑戰賽 - 梁一貞 !';youtubeId=-6iOarduSoY  erb playlist.html.erb > docs/playlist/episode/334/明星/198/index.html
